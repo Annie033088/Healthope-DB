ee317cfe75ec2affe8eb2398473103cad28c2481
Annie <Annie2000890308@gmail.com>
2025-04-19 17:42:35 +0800

    測試還原專案流程

--- HealthopeDatabase/HealthopeDatabase.Migration/InitializeSchemaVersionsTable.sql
+++ HealthopeDatabase/HealthopeDatabase.Migration/InitializeSchemaVersionsTable.sql
@@ -1,5 +1,6 @@
 -- DbUp SchemaVersions Backup
--- Generated on: 2025/4/18 下午 05:13:31
+-- Generated on: 2025/4/19 下午 05:39:38
 
-INSERT INTO SchemaVersions (ScriptName, Applied) VALUES ('20250418_EditAdminTable.sql', '2025-04-18 15:52:21');
+INSERT INTO SchemaVersions (ScriptName, Applied) VALUES ('20250418_editAdminTable.sql', '2025-04-18 15:52:21');
 INSERT INTO SchemaVersions (ScriptName, Applied) VALUES ('20250418_addMemberTable.sql', '2025-04-18 17:13:31');
+INSERT INTO SchemaVersions (ScriptName, Applied) VALUES ('20250419_addMemberAccessRecordTable.sql', '2025-04-19 17:39:38');
---
+++ HealthopeDatabase/HealthopeDatabase.Migration/SQLScripts/20250419_addMemberAccessRecordTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE t_memberAccessRecord(
+	f_memberAccessRecordId INT PRIMARY KEY IDENTITY,
+	f_memberId INT NOT NULL,
+	f_entryTime DATETIME NOT NULL,
+	f_exitTime DATETIME NOT NULL DEFAULT '1900-01-01 00:00:00'
+	CONSTRAINT fk_memberAccessRecordMemberId FOREIGN KEY (f_memberId) REFERENCES t_member(f_memberId)
+)
