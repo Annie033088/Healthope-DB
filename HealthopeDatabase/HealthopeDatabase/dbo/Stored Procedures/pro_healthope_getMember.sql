9f292bfa043d5dc789fe26e3a21bfe49af13ccd1
Annie <Annie2000890308@gmail.com>
2025-05-09 18:30:34 +0800

    2025-05-09 DB: 1.增加 SP:查詢會員 2.增加 SP:根據 id 取得修改會員時需要的資料

---
+++ HealthopeDatabase/HealthopeDatabase/dbo/Stored Procedures/pro_healthope_getMember.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE pro_healthope_getMember
+	@searchName VARCHAR(50),
+	@searchPhone INT,
+	@status BIT,
+	@sortOrder VARCHAR(10),
+	@sortOption VARCHAR(7),
+	@recordPerPage TINYINT,
+	@page INT,
+	@totalPage INT OUTPUT
+AS
+BEGIN
+    -- 計算分頁起始筆數
+    DECLARE @offset INT = (@page - 1) * @recordPerPage;
+    DECLARE @totalRecord INT;
+
+	SELECT f_memberId, f_name, f_phone, f_phoneVerified, f_email, f_membershipExpiry, f_status, f_absenceTime, f_allowGroupClass
+	INTO #tempMemberTable
+    FROM t_member WITH(NOLOCK)
+    WHERE (@searchName IS NULL OR f_name LIKE '%' + @searchName + '%')
+      AND (@status IS NULL OR f_status = @status)
+      AND (@searchPhone IS NULL OR f_phone LIKE '%' + CAST(@searchPhone AS VARCHAR))
+
+	SELECT @totalRecord = COUNT(*) FROM #tempMemberTable WITH(NOLOCK); 
+	SET @totalPage = CEILING(@totalRecord * 1.0 / @recordPerPage); --總頁數
+
+	SELECT f_memberId, f_name, f_phone, f_phoneVerified, f_email, f_membershipExpiry, f_status, f_absenceTime, f_allowGroupClass
+    FROM #tempMemberTable WITH(NOLOCK)
+    ORDER BY
+       CASE WHEN @sortOption IS NULL AND  @sortOrder = 'ascending' THEN CAST(f_memberId AS sql_variant) END ASC,
+	   CASE	WHEN @sortOption IS NULL AND  @sortOrder = 'descending' THEN CAST(f_memberId AS sql_variant) END DESC,
+       CASE WHEN @sortOption = 'name' AND @sortOrder = 'ascending' THEN CAST(f_name AS sql_variant) END ASC,
+       CASE WHEN @sortOption = 'name' AND @sortOrder = 'descending' THEN CAST(f_name AS sql_variant) END DESC,
+       CASE WHEN @sortOption = 'status' AND @sortOrder = 'ascending' THEN CAST(f_status AS sql_variant) END ASC,
+       CASE WHEN @sortOption = 'status' AND @sortOrder = 'descending' THEN CAST(f_status AS sql_variant) END DESC,
+       CASE WHEN @sortOption = 'membershipExpiry' AND @sortOrder = 'ascending' THEN CAST(f_membershipExpiry AS sql_variant) END ASC,
+       CASE WHEN @sortOption = 'membershipExpiry' AND @sortOrder = 'descending' THEN CAST(f_membershipExpiry AS sql_variant) END DESC,
+       CAST(f_memberId AS SQL_VARIANT)  ASC --預設排序欄位
+    OFFSET @offset ROWS FETCH NEXT @recordPerPage ROWS ONLY;
+END
---
+++ HealthopeDatabase/HealthopeDatabase/dbo/Stored Procedures/pro_healthope_getMemberEditDataById.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE pro_healthope_getMemberEditDataById
+@memberId INT
+AS
+BEGIN
+	SELECT f_name, f_phone, f_status, f_updateTime
+    FROM t_member WITH(NOLOCK)
+	WHERE f_memberId = @memberId
+END
