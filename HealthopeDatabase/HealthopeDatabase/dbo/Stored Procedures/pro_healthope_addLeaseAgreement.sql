2ee80a2d0f1e43ab82079fabe08aeb86f3eb97f4
Annie <Annie2000890308@gmail.com>
2025-06-03 18:08:04 +0800

    2025-06-03 DB: 1.完成 SP:新增租約 2.完成 SP:取得租約

---
+++ HealthopeDatabase/HealthopeDatabase/dbo/Stored Procedures/pro_healthope_addLeaseAgreement.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE pro_healthope_addLeaseAgreement
+@startTime DATE,
+@endTime DATE,
+@reminderLeadTime INT
+AS
+BEGIN
+	INSERT INTO t_leaseAgreement(f_startTime, f_endTime, f_reminderLeadTime) 
+	VALUES(@startTime, @endTime, @reminderLeadTime)
+END
---
+++ HealthopeDatabase/HealthopeDatabase/dbo/Stored Procedures/pro_healthope_getLeaseAgreement.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE pro_healthope_getLeaseAgreement
+	@status TINYINT, 
+	@recordPerPage INT,
+	@page INT,
+	@totalPage INT OUTPUT
+AS
+BEGIN
+    -- 計算分頁起始筆數
+    DECLARE @offset INT = (@page - 1) * @recordPerPage;
+    DECLARE @totalRecord INT;
+
+	SELECT f_leaseAgreementId, f_startTime, f_endTime, f_reminderLeadTime, f_status, f_remind, f_remark, f_updateTime
+	INTO #tempLeaseAgreementTable
+    FROM t_leaseAgreement WITH(NOLOCK)
+    WHERE  (@status IS NULL OR f_status = @status)
+
+	SELECT @totalRecord = COUNT(*) FROM #tempLeaseAgreementTable WITH(NOLOCK); 
+	SET @totalPage = CEILING(@totalRecord * 1.0 / @recordPerPage);
+
+	SELECT f_leaseAgreementId, f_startTime, f_endTime, f_reminderLeadTime, f_status, f_remind, f_remark, f_updateTime
+    FROM #tempLeaseAgreementTable WITH(NOLOCK)
+    ORDER BY f_leaseAgreementId
+    OFFSET @offset ROWS FETCH NEXT @recordPerPage ROWS ONLY;
+END
